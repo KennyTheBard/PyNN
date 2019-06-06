eea71f881d0c09146411188a657e5ccb968cf51a
KennyTheBard <octav_teculescu@yahoo.com>
2019-06-06 11:25:28 +0300

    Still not working properly

--- Own Try 1/nn.py
+++ Own Try 1/nn.py
@@ -1,13 +1,14 @@
 import numpy as np
 
 class NeuralNetwork(object):
-	def __init__(self, inputSize, hiddenSize, outputSize):
-		self.inputSize = inputSize
-		self.hiddenSize = hiddenSize
-		self.outputSize = outputSize
-
-		self.w1 = np.random.randn(self.inputSize, self.hiddenSize)
-		self.w2 = np.random.randn(self.hiddenSize, self.outputSize)
+	def __init__(self, inputSize, hiddenLayers, outputSize):
+		self.layers = [inputSize] + hiddenLayers + [outputSize]
+		print self.layers
+		self.weights = [0] * (len(self.layers) - 1)
+		for i in range(len(self.weights)):
+			self.weights[i] = np.random.randn(self.layers[i], self.layers[i + 1])
+		self.zs = [0] * len(self.weights)
+		self.deltas = [0] * len(self.weights)
 
 	def sigmoid(self, s):
 		return 1 / (1 + np.exp(-s))
@@ -16,16 +17,27 @@ class NeuralNetwork(object):
 		return s / (1 - s)
 
 	def forward(self, x):
-		self.z1 = self.sigmoid(np.dot(x, self.w1))
-		self.z2 = self.sigmoid(np.dot(self.z1, self.w2))
-		return self.z2
+		for i in range(len(self.weights)):
+			if i == 0:
+				self.zs[i] = self.sigmoid(np.dot(x, self.weights[i]))
+			else :
+				self.zs[i] = self.sigmoid(np.dot(self.weights[i - 1], self.weights[i]))
+		return self.zs[-1]
 
 	def backward(self, x, y, out):
-		self.out_delta = (y - out) * self.sigmoidPrime(out)
-		self.z2_delta = np.dot(self.out_delta, self.w2.T) * self.sigmoidPrime(self.z2)
+		for i in reversed(range(len(self.weights))):
+			if i == len(self.weights) - 1:
 
-		self.w1 += x.T.dot(self.z2_delta)
-		self.w2 += self.z2.T.dot(self.out_delta)
+				self.deltas[i] = (y - out) * self.sigmoidPrime(out)
+			else :
+				self.deltas[i] = np.dot(self.deltas[i + 1], self.weights[i + 1].T) * self.sigmoidPrime(self.zs[i + 1])
+
+		for i in range(len(self.weights)):
+			if i == 0:
+				self.weights[i] += x.T.dot(self.deltas[i])
+			else:
+				self.weights[i] += self.zs[i].T.dot(self.deltas[i])
+
 
 	def train(self, x, y):
 		out = self.forward(x)
@@ -35,16 +47,17 @@ class NeuralNetwork(object):
 		print str(self.forward(x))
 
 
-def f(x, y):
-	return np.exp(1 - x * x - y * y)
+def f(x, y, z):
+	return 5 * x - 2 * y + z + 1
 
 list_x = []
 list_y = []
 for i in range(20):
-	x = np.random.uniform(-3, 3)
-	y = np.random.uniform(-3, 3)
-	list_x.append([x, y])
-	list_y.append([f(x, y)])
+	x = np.random.uniform(-10, 10)
+	y = np.random.uniform(-10, 10)
+	z = np.random.uniform(-10, 10)
+	list_x.append([x, y, z])
+	list_y.append([f(x, y, z)])
 
 x = np.asarray(list_x, dtype = float)
 x = x / np.amax(x, axis = 0)
@@ -52,12 +65,10 @@ x = x / np.amax(x, axis = 0)
 y = np.asarray(list_y, dtype = float)
 y = y / np.amax(y, axis = 0)
 
-NN = NeuralNetwork(2, 7, 1)
+NN = NeuralNetwork(3, [7], 1)
 
-for u in xrange(10000):
-	NN.train(x, y)
-	if u % 100 == 0:
-		pred = np.array(([1, 1]), dtype = float)
-		pred = pred / np.amax(pred, axis = 0)
-		NN.predict(pred)
-		print "Correct: " + str(f(1, 1))
+for u in xrange(100):
+	NN.train(x[u % len(x)], y[u % len(y)])
+	if u % 10 == 0:
+		NN.predict(x[0])
+		print "Correct: " + str(y[0]) + "\n"
